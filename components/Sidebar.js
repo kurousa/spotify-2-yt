b4174baba45fdb398435eef2ca5c2561d3b0a18f
kurousa <r.takahashi.1026@gmail.com>
2021-12-02 20:56:08 +0900

    mod sidebar style

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,4 @@
 import {
-    HeartIcon,
     HomeIcon, 
     LibraryIcon, 
     LogoutIcon, 
@@ -7,6 +6,9 @@ import {
     RssIcon, 
     SearchIcon,
 } from "@heroicons/react/outline";
+import {
+    HeartIcon,
+} from "@heroicons/react/solid";
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
@@ -34,38 +36,34 @@ function Sidebar() {
         <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[50rem] hidden md:inline-flex pb-36">
             <div className="space-y-4">
                 <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
-                    <LogoutIcon className="h-5 w-5"/>
+                    <LogoutIcon className="sidebar-button"/>
                     <p>Log Out</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <HomeIcon className="h-5 w-5"/>
+                    <HomeIcon className="sidebar-button"/>
                     <p>Home</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <SearchIcon className="h-5 w-5"/>
+                    <SearchIcon className="sidebar-button"/>
                     <p>Search</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <LibraryIcon className="h-5 w-5"/>
+                    <LibraryIcon className="sidebar-button"/>
                     <p>Your Library</p>
                 </button>
 
                 <hr className="border-t-[0.1]px border-gray-900"/>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <PlusCircleIcon className="h-5 w-5"/>
+                    <PlusCircleIcon className="sidebar-button"/>
                     <p>Create Playlist</p>
                 </button>
-                <button className="flex items-center space-x-2 hover:text-white">
-                    <HeartIcon className="h-5 w-5"/>
+                <button className="flex items-center space-x-2 hover:text-white ">
+                    <HeartIcon className="sidebar-button text-blue-500"/>
                     <p>Your episodes</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <LibraryIcon className="h-5 w-5"/>
-                    <p>Liked Songs</p>
-                </button>
-                <button className="flex items-center space-x-2 hover:text-white">
-                    <RssIcon className="h-5 w-5"/>
+                    <RssIcon className="sidebar-button text-green-500"/>
                     <p>Your episodes</p>
                 </button>
 
